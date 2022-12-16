c407eb3f87183fdf5f3f1b0a405d96d049aeb115
kumar.anand <kumar.anand@kambala.co.in>
2022-12-16 13:37:11 +0530

    updated dependency NorenApi-java-2.0.0.jar price type fix

--- src/main/java/com/kspl/norenexampleapp/NorenExampleApp.java
+++ src/main/java/com/kspl/norenexampleapp/NorenExampleApp.java
@@ -17,16 +17,16 @@ import org.json.JSONObject;
 public class NorenExampleApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello and Welcom to Noren World!");
-        NorenApiJava api = new com.noren.javaapi.NorenApiJava("http://kurma.kambala.co.in:9959/NorenWClient/");
+        System.out.println("Hello and Welcome to Noren!");
+        NorenApiJava api = new com.noren.javaapi.NorenApiJava("http://matsya.kambala.co.in:9959/NorenWClient/");
         
-        String response = api.login("MOBKUMAR", "Zxc@1234", "01-01-1970", "IDART_DESK", "12be8cef3b1758f5", "java-");
+        String response = api.login("MOBKUMAR", "Qwe@1234", "01-01-1970", "IDART_DESK", "12be8cef3b1758f5", "java-");
         System.out.println(response);
             
         JSONObject search_reply = api.search("NSE", "TCS"); 
         System.out.println(search_reply.toString());
         
-        JSONObject reply = api.place_order("B","I", "NSE", "CANBK-EQ", 1, 0, "L", 220.0, "java", null, null, null, null, null, null); 
+        JSONObject reply = api.place_order("B","I", "NSE", "CANBK-EQ", 1, 0, "LMT", 220.0, "java", null, null, null, null, null, null); 
         System.out.println(reply.toString());
         
         JSONArray book; 
